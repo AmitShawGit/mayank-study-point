88a8abe828da1477b245c387906063f79e04b173
Amit <amitshaw1421@gmail.com>
2023-09-20 22:51:37 +0530

    testimonial added and hero video changed

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -33,16 +33,18 @@ if(this.value === "all"){
 })
 
 /* Owl Carousel */
-$('.testimonial').owlCarousel({
+$('.owl-carousel').owlCarousel({
   loop:true,
   margin:10,
   nav:true,
+  autoplay:true,
+  autoplayTimeout:2000,
   responsive:{
       0:{
           items:1
       },
       600:{
-          items:2
+          items:1
       },
       1000:{
           items:2
